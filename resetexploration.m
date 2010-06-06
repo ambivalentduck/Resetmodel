b770d55e767a9279218026210825dcebaf89e4a9
Justin <justin@home>
2010-06-06 17:42:41 -0500

    First commit after branching

--- resetexploration.m
+++ resetexploration.m
@@ -21,26 +21,6 @@ ik=ikin(p);
 t=0:.01:1;
 x=minjerk([-.2 -.2],[.2 .2],[0 0],[0 0],t);
 T=length(t);
-rxpast=zeros(2,T);
 
-figure(1)
-for k=1:T
-    clf
-    set(gca,'xlim',[-1 1])
-    set(gca,'ylim',[-1 1])
-    hold on
-    plot(x(1,1:k),x(2,1:k),'b-')
-    plot(x(1,k),x(2,k),'bx')
-    plot(x0(1),x0(2),'rx')
 
-    it=ikin(x(:,k));
-    xt=fkin(it);
-    plot(xt(1),xt(2),'r.')
-    rxpast(:,k)=xt;
-    x1=[x0(1)+l1*cos(it(1));x0(2)+l1*sin(it(1))];
-    plot([x0(1),x1(1)],[x0(2),x1(2)],'r-')
-    plot([x1(1),xt(1)],[x1(2),xt(2)],'r-')
-    plot(rxpast(1,1:k),rxpast(2,1:k),'gx')
-    F(k)=getframe;
-end
-movie(F)
+
